65e7babf2d7f2d5cac42481ac0a095157ef74565
ixnp <enprocopios@gmail.com>
2020-02-26 08:15:12 -0800

    adding associations

---
+++ 19-rails-associations/app/controllers/tacos_controller.rb
@@ -0,0 +1,45 @@
+class TacosController < ApplicationController
+    def index 
+        @tacos = Taco.all
+    end 
+    
+    def show
+        @taco = Taco.find_by_id(params[:id])
+    end 
+
+    def new 
+        @taco = Taco.new
+        @trucks = Truck.all
+    end 
+
+    def create
+        
+        @taco = Taco.create(taco_params)
+        if @taco.valid?
+            redirect_to tacos_path
+        else
+            flash[:messages] = @taco.errors.full_messages
+            redirect_to new_taco_path
+        end 
+
+    end 
+
+    def edit
+        @taco = Taco.find_by_id(params[:id])
+        @trucks = Truck.all
+
+    end 
+
+    def update
+        @taco = Taco.find_by_id(params[:id])
+        @taco.update(taco_params)
+        redirect_to tacos_path
+        
+    end 
+
+    private 
+
+        def taco_params
+            params.require(:taco).permit(:meat,:zest,:spicy, :truck_id)
+        end
+end
---
+++ 19-rails-associations/db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Truck.create(name:"tacos4us",location:"seattle")
+Truck.create(name:"tacos?",location:"portland")
+Truck.create(name:"7",location:"NYC")
+
+Taco.create(meat:"chicken", zest:2, spicy:4, truck_id:1)
+Taco.create(meat:"pork", zest:5, spicy:1, truck_id:2)
+Taco.create(meat:"beef", zest:4, spicy:5, truck_id:3)
